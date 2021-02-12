b1230e15ee50215a45a91970db8b86a2107487ad
Daniel Persson <persson.daniel.1990@gmail.com>
2021-02-12 23:17:29 +0100

    Add data to database through code

---
+++ src/main/java/com/example/springbootpractise/student/StudentConfig.java
@@ -0,0 +1,40 @@
+package com.example.springbootpractise.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+import static java.time.Month.*;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository) {
+        return args -> {
+            Student daniel = new Student(
+                    1L,
+                    "Daniel",
+                    "daniel.persson@frontedgeit.se",
+                    LocalDate.of(1990, FEBRUARY, 12),
+                    31
+            );
+
+            Student john = new Student(
+                    2L,
+                    "John",
+                    "john.doe@frontedgeit.se",
+                    LocalDate.of(1995, FEBRUARY, 15),
+                    25
+            );
+
+            repository.saveAll(
+                    List.of(daniel, john)
+            );
+        };
+    }
+}
